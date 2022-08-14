82eae0b1709ec3897b9cb6184c9bfe301161e5fa
madahetooo <ieslammedhat@gmail.com>
2022-08-14 20:15:24 +0200

    Updated main.dart file
    Adding print message

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: (){
+          print("Update Code");
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
